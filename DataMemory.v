dfc344b05fb8354927333d775f0c94d92298cc4d
DIEGOJIM13 <diegojimenez@email.arizona.edu>
2016-10-23 17:25:20 -0700

    Add files via upload

--- DataMemory.v
+++ DataMemory.v
@@ -35,14 +35,14 @@
 // of the "Address" input to index any of the 256 words. 
 ////////////////////////////////////////////////////////////////////////////////
 
-module DataMemory(Address, WriteData, Clk, MemWrite, MemRead, ReadData); 
+module DataMemory(Address, WriteData, Clk, MemWrite, MemRead, ReadData, MemOp); 
 
     input [31:0] Address; 	// Input Address 
     input [31:0] WriteData; // Data that needs to be written into the address 
     input Clk;
     input MemWrite; 		// Control signal for memory write 
     input MemRead; 			// Control signal for memory read 
-    
+    input [3:0] MemOp;
     reg [31:0] MemArray [0:1023];
 
     output reg [31:0] ReadData; // Contents of memory location at Address
@@ -52,17 +52,85 @@ module DataMemory(Address, WriteData, Clk, MemWrite, MemRead, ReadData);
     initial begin
         for (i=10'b0000000000; i<=10'b1111111111; i = i+10'b0000000001) begin
             MemArray[i] = 32'h00000000;
-        end    
+        end
+        //.word 0, 1, 2, 3, 4 , -1
+        MemArray[0] <= 32'd0;
+        MemArray[1] <= 32'd1;
+        MemArray[2] <= 32'd2;
+        MemArray[3] <= 32'd3;
+        MemArray[4] <= 32'd4;
+        MemArray[5] <= -32'd1;        
     end
     
     always @(posedge Clk) begin
         if (MemWrite == 1'b1)
-            MemArray[Address[11:2]] <= WriteData;
+            if (MemOp[3:0] == 4'b1000) begin  //sh
+                if (Address[1:0] == 2'b00) begin
+                    MemArray[Address[11:2]][15:0] <= WriteData[15:0];
+                end
+                else if (Address[1:0] == 2'b00) begin
+                    MemArray[Address[11:2]][23:8] <= WriteData[15:0];
+                end
+                else if (Address[1:0] == 2'b00) begin
+                    MemArray[Address[11:2]][31:15] <= WriteData[15:0];
+                end
+                else begin
+                    MemArray[Address[11:2]][15:0] <= WriteData[15:0]; //Not sure
+                end               
+            end
+            else if (MemOp[3:0] == 4'b0100) begin //sb
+                if (Address[1:0] == 2'b00) begin
+                    MemArray[Address[11:2]][7:0] <= WriteData[7:0];
+                end
+                else if (Address[1:0] == 2'b01) begin
+                    MemArray[Address[11:2]][15:8] <= WriteData[7:0];
+                end
+                else if (Address[1:0] == 2'b10) begin
+                    MemArray[Address[11:2]][23:16] <= WriteData[7:0];
+                end
+                else begin 
+                    MemArray[Address[11:2]][31:24] <= WriteData[7:0]; //Not sure
+                end
+                
+            end
+            else  if (MemOp[3:0] == 4'b1100)begin //sw
+                MemArray[Address[11:2]] <= WriteData;
+            end
     end
     
     always @(MemRead) begin
-        if (MemRead == 1)
-            ReadData <= MemArray[Address[11:2]];
+        if (MemRead == 1) begin
+            if (MemOp[3:0] == 4'b0100) begin //lh
+                if (Address[1:0] == 2'b00) begin
+                    ReadData <= {{16{MemArray[Address[11:2]][15]}},{MemArray[Address[11:2]][15:0]}};                
+                end
+                else if (Address[1:0] == 2'b01) begin
+                    ReadData <= {{16{MemArray[Address[11:2]][23]}},{MemArray[Address[11:2]][23:8]}}; 
+                end                
+                else if (Address[1:0] == 2'b10) begin
+                    ReadData <= {{16{MemArray[Address[11:2]][31]}},{MemArray[Address[11:2]][31:16]}};                 
+                end            
+                else begin
+                    ReadData <= {{16{MemArray[Address[11:2]][15]}},{MemArray[Address[11:2]][15:0]}}; 
+                end            
+            end
+            else if (MemOp[3:0] == 4'b0001) begin  //lb
+                if (Address[1:0] == 2'b00) begin
+                    ReadData <= {{24{MemArray[Address[11:2]][7]}},{MemArray[Address[11:2]][7:0]}};                
+                end
+                else if (Address[1:0] == 2'b01) begin
+                    ReadData <= {{24{MemArray[Address[11:2]][15]}},{MemArray[Address[11:2]][15:8]}}; 
+                end                
+                else if (Address[1:0] == 2'b10) begin
+                    ReadData <= {{24{MemArray[Address[11:2]][23]}},{MemArray[Address[11:2]][23:16]}};                 
+                end            
+                else begin
+                    ReadData <= {{24{MemArray[Address[11:2]][31]}},{MemArray[Address[11:2]][31:24]}}; 
+                end
+            end
+            else if (MemOp[3:0] == 4'b0011)begin //lw
+                ReadData <= MemArray[Address[11:2]];
+            end
+        end   
     end
-
 endmodule
